65114eb744cb2f4feccc2766bf25f730eef08260
huojian <36457050@qq.com>
2019-10-12 13:57:29 +0800

    调整.xml文件位置，修改前台参数输入形式

--- src/main/java/com/example/happy/controller/PersonManageController.java
+++ src/main/java/com/example/happy/controller/PersonManageController.java
@@ -3,6 +3,7 @@ package com.example.happy.controller;
 import com.example.happy.domain.PersonInfo;
 import com.example.happy.service.PersonManageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,8 +16,8 @@ public class PersonManageController {
     @Autowired
     private PersonManageService personManageService;
 
-    @RequestMapping(value = "/insert")
-    public String insert(int id) {
+    @RequestMapping(value = "/insert/{id}")
+    public String insert(@PathVariable("id") int id) {
         PersonInfo person = new PersonInfo();
         person.setId(id);
         person.setName("测试" + id);
@@ -24,17 +25,17 @@ public class PersonManageController {
         return "insert success";
     }
 
-    @RequestMapping(value = "/queryOne")
-    public String queryOne(int id) {
+    @RequestMapping(value = "/queryOne/{id}")
+    public String queryOne(@PathVariable("id") int id) {
         PersonInfo person = personManageService.queryPersonById(id);
         return "person.getId()=" + person.getId() + "person.getName()=" + person.getName();
     }
 
-    @RequestMapping(value = "/update")
-    public String update(int id) {
+    @RequestMapping(value = "/update/{id}")
+    public String update(@PathVariable("id") int id) {
         PersonInfo person = new PersonInfo();
         person.setId(id);
-        person.setName("测试" + id + "改");
+        person.setName(id + "改");
         personManageService.updatePerson(person);
         return "update success....";
     }
@@ -48,7 +49,7 @@ public class PersonManageController {
         List<PersonInfo> list = personManageService.queryPersonList();
         String str = "";
         for (PersonInfo person : list) {
-            str = str + ("person.getId()=" + person.getId()+"+person.getName()="+person.getName());
+            str = str + ("ID=" + person.getId()+",NAME="+person.getName()+"；");
         }
         return str;
     }
